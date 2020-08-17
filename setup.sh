9cd8326468f31d252ea3686c43d1b825dc8d202b
Shuhao <ShuhaoLai18@gmail.com>
2020-08-17 00:47:02 -0400

    Set up test code

---
+++ setup.sh
@@ -0,0 +1,5 @@
+# Downloads packages needed for this package
+# Original built for Linux (Ubuntu) device
+# Prerequisites: Anaconda
+conda install pytorch torchvision cudatoolkit=10.2 -c pytorch
+conda install matplotlib
