f9c31e97b63021e639f8cf66269dbd262a98a876
Shuhao <ShuhaoLai18@gmail.com>
2020-09-17 22:25:11 -0400

    Test worked

--- streamline/startup.sh
+++ streamline/startup.sh
@@ -23,8 +23,8 @@ cd GCP_AI
 
 python MNIST_test.py
 
-#export NAME=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/name -H 'Metadata-Flavor: Google')
-#export ZONE=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/zone -H 'Metadata-Flavor: Google')
-#gcloud --quiet compute instances delete $NAME --zone=$ZONE
+export NAME=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/name -H 'Metadata-Flavor: Google')
+export ZONE=$(curl -X GET http://metadata.google.internal/computeMetadata/v1/instance/zone -H 'Metadata-Flavor: Google')
+gcloud --quiet compute instances delete $NAME --zone=$ZONE
 echo "------------------------------------------FINISHED--------------------------------------------------"
 
